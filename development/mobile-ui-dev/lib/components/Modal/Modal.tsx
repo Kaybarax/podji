4abe1a1747620dce139221c8c306497ea0635b09
Kaybarax <Kaybarax@users.noreply.github.com>
2025-06-30 10:57:06 +0200

    Add support for `sass-embedded` and improve SASS theme variables:
    - Installed `sass-embedded` to streamline SASS compilation and theming workflows.
    - Added light and dark theme-specific SASS variables for typography, colors, and spacing in `generateWebStylesheets.js`.
    - Adjusted `yarn.lock` to reflect new package dependencies.

--- development/mobile-ui-dev/lib/components/Modal/Modal.tsx
+++ development/mobile-ui-dev/lib/components/Modal/Modal.tsx
@@ -202,7 +202,7 @@ export const Modal: React.FC<ModalProps> = ({
 
     const content = (
       <Animated.View style={[contentPositionStyle, contentAnimatedStyle, contentStyle]} testID={testID}>
-        {scrollable ? <ScrollView>{children}</ScrollView> : <>{children}</>}
+        {scrollable ? <ScrollView><>{children}</></ScrollView> : <>{children}</>}
       </Animated.View>
     );
 
