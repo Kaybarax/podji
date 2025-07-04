ee6e09b8c647665dcb92057bd62a730839b99da1
Kaybarax <Kaybarax@users.noreply.github.com>
2025-07-04 22:42:44 +0200

    Refactor Vite configuration for schemas package
    - Updated `dts` plugin settings to include additional options and refine type declaration generation.
    - Modified `build` configuration to adjust output directory, file naming, and rollup options.

--- packages/schemas/vite.config.ts
+++ packages/schemas/vite.config.ts
@@ -4,12 +4,25 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({ include: ['lib'] })],
+  plugins: [
+    dts({ 
+      include: ['lib/**/*'],
+      outDir: 'dist',
+      insertTypesEntry: true,
+      rollupTypes: false,
+      copyDtsFiles: false,
+      staticImport: true,
+      clearPureImport: true,
+      entryRoot: 'lib'
+    })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
       formats: ['es'],
+      fileName: 'schemas'
     },
+    outDir: 'dist',
     copyPublicDir: false,
     rollupOptions: {
       onwarn(warning, warn) {
